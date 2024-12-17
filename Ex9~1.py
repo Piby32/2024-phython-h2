05e2531ca1c3162c79ae7f9153d0eebaaa373586
Piby32 <piby1216@gmail.com>
2024-12-17 19:48:31 +0800

    Create Ex9~1.py

---
+++ Ex9~1.py
@@ -0,0 +1,30 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+#--------------------------------------------------#
+class Person:
+  def __init__(self, name, age, birthday):
+    self.name = name
+    self.age = age
+    self.birthday = birthday
+p1 = Person("name:piby", "age:12", "birthday:12.16")
+
+print(p1.name)
+print(p1.age)
+print(p1.birthday)
+
+
+
+
+
+
+
+
+
+
